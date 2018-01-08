1524dcee7b3b3ae72b8a85dbbbf79065a1b173d0
Unknown <matei.oltean@gmail.com>
2018-01-08 23:06:08 +0100

    Id to be given as an argument

--- Gossiper/tools/Tasks/TaskMap.go
+++ Gossiper/tools/Tasks/TaskMap.go
@@ -23,28 +23,17 @@ func (tm TaskMap) getTasks(p string) []Task {
 	return tm.Tasks[p]
 }
 
-func (tm TaskMap) addTask(peer string, origin string, mat1 gomatcore.Matrix, mat2 gomatcore.Matrix) (id uint32) {
+func (tm TaskMap) addTask(peer string, origin string, mat1 gomatcore.Matrix, mat2 gomatcore.Matrix, id uint32) {
 	tm.Lock.Lock()
 	defer tm.Lock.Unlock()
 	if _, ok := tm.Tasks[peer]; !ok {
 		tm.Tasks[peer] = make([]Task, 1)
-	}
-	id = uint32(0)
-
-	for _, t := range tm.Tasks[peer] {
-		if t.ID > id {
-			id = t.ID
-		}
-	}
-	if id > 0 {
-		id++
 	}
 	tm.Tasks[peer] = append(tm.Tasks[peer],
 		Task{ID: id,
 			Origin: origin,
 			Mat1: mat1,
 			Mat2: mat2,})
-	return
 }
 
 func (tm TaskMap) removeTask(peer string, id uint32) {
