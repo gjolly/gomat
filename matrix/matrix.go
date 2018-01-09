c68a7a9b6c01f5ff5a5adcb7e2083740f596eaf4
Kevin C <kevin.carenou@etu.inp-toulouse.fr>
2018-01-09 19:26:45 +0100

    Fix matrix type

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -7,49 +7,55 @@ import (
 )
 
 // Matrix represents a matrix using the conventional storage scheme.
-type Matrix struct {
-	*mat.Dense
+type Matrix mat.Dense
+
+func (m *Matrix) toDense() *mat.Dense {
+	mDense := mat.Dense(*m)
+	return &mDense
 }
 
 // New creates a new Matrix
 func New(r, c int, data []float64) *Matrix {
-	return &Matrix{mat.NewDense(r, c, data)}
+	m := Matrix(*mat.NewDense(r, c, data))
+	return &m
 }
 
 // ToString returns the string representation of the matrix.
 func (m *Matrix) ToString() string {
-	return fmt.Sprintf("%v", mat.Formatted(m, mat.Squeeze()))
+	return fmt.Sprintf("%v", mat.Formatted(m.toDense(), mat.Squeeze()))
 }
 
 // Equal returns whether the matrices a and b have the same size and are element-wise equal
 func Equal(m1, m2 *Matrix) bool {
-	return mat.Equal(m1, m2)
+	return mat.Equal(m1.toDense(), m2.toDense())
 }
 
-func AddMatrix(m1, m2 *Matrix) *Matrix {
-	r, c := m1.Dims()
-	m := New(r, c, nil)
-	m.Add(m1, m2)
-	return m
+func Add(m1, m2 *Matrix) *Matrix {
+	r, c := m1.toDense().Dims()
+	m := mat.NewDense(r, c, nil)
+	m.Add(m1.toDense(), m2.toDense())
+	res := Matrix(*m)
+	return &res
 }
 
-func SubMatrix(m1, m2 *Matrix) *Matrix {
-	r, c := m1.Dims()
-	m := New(r, c, nil)
-	m.Sub(m1, m2)
-	return m
+func Sub(m1, m2 *Matrix) *Matrix {
+	r, c := m1.toDense().Dims()
+	m := mat.NewDense(r, c, nil)
+	m.Sub(m1.toDense(), m2.toDense())
+	res := Matrix(*m)
+	return &res
 }
 
-func MulMatrix(m1, m2 *Matrix) *Matrix {
-	r, _ := m1.Dims()
-	_, c := m2.Dims()
-	m := New(r, c, nil)
-	m.Mul(m1, m2)
-	return m
+func Mul(m1, m2 *Matrix) *Matrix {
+	r, c := m1.toDense().Dims()
+	m := mat.NewDense(r, c, nil)
+	m.Mul(m1.toDense(), m2.toDense())
+	res := Matrix(*m)
+	return &res
 }
 
-func (m Matrix) MaxDim() int {
-	x, y := m.Dims()
+func MaxDim(m Matrix) int {
+	x, y := m.toDense().Dims()
 	if x > y {
 		return x
 	}
