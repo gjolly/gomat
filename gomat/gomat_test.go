39fa84cca56cd5e9bdd7fed5f9e262d4c0b4c03d
Gauthier Jolly <contact@gauthierjolly.com>
2018-01-09 21:23:01 +0100

    API works (if protobuf works) for sum and substaction

--- gomat/gomat_test.go
+++ gomat/gomat_test.go
@@ -6,41 +6,49 @@ import (
 	"net"
 	"github.com/matei13/gomat/Gossiper/tools/Messages"
 	"github.com/dedis/protobuf"
-	"log"
 	"github.com/matei13/gomat/matrix"
+	"time"
+	"fmt"
 )
 
 func daemon() {
-	c, err := net.Dial("unix", "/tmp/gomat.sock")
+	l, err := net.Listen("unix", "/tmp/gomat.sock")
 	if err != nil {
 		panic(err)
 	}
 	requestBuf := make([]byte, 65507)
 
+	c, _ := l.Accept()
 	nb, _ := c.Read(requestBuf)
 	requestMess := Messages.RumorMessage{}
-	protobuf.Decode(requestBuf[0:nb], &requestMess)
-	log.Println(requestMess)
+	err = protobuf.Decode(requestBuf[0:nb], &requestMess)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(requestMess)
 
 	r := matrix.New(2, 2, []float64{3, 3, 3, 3})
-	responseMess := Messages.RumorMessage{"", 0, *r, *r, Messages.Sum, "", "", 0}
+	r2 := matrix.New(2, 2, []float64{3, 3, 3, 3})
+	responseMess := Messages.RumorMessage{"", 0, *r, *r2, Messages.Sum, "", "", 0}
 	responseBuf, _ := protobuf.Encode(&responseMess)
 
 	c.Write(responseBuf)
-
+	c.Close()
+	l.Close()
 }
 
 func TestAdd(t *testing.T) {
 	m1 := matrix.New(2, 2, []float64{2, 2, 2, 2})
 	m2 := matrix.New(2, 2, []float64{1, 1, 1, 1})
-	r, err := Add(*m1, *m2)
+	r, err := Add(m1, m2)
 	if err != nil {
 		panic(err)
 	}
-	log.Println(r)
+	fmt.Println(r)
 }
 
 func TestMain(m *testing.M) {
 	go daemon()
+	time.Sleep(time.Second)
 	os.Exit(m.Run())
 }
